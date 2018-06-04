5e6d32cbabdb0a87dd15777c1f76ae6627b15893
Allan Hung <hung.allan@gmail.com>
2018-06-04 18:49:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# pyxdump
+
+MySQL schema dump tool
+
+## How to Build
+```sh
+$ pip install git+https://github.com/allan-hung/pyxdump
+```
+    
+## Example
+```
+### dump
+```sh
+$ pyxdump dump schema --user root --password dbpass
+```
+
+### import
+```sh
+$ pyxdump import schema --script_file /tmp/alldb.sql
+$ pyxdump import data --backupdir /dbbackup/20180604 --datadir /data/mysql
+```
---
+++ setup.py
@@ -0,0 +1,77 @@
+try:
+    import setuptools
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def discover_and_run_tests():
+    import os
+    import sys
+    import unittest
+
+    # get setup.py directory
+    setup_file = sys.modules['__main__'].__file__
+    setup_dir = os.path.abspath(os.path.dirname(setup_file))
+
+    # use the default shared TestLoader instance
+    test_loader = unittest.defaultTestLoader
+
+    # use the basic test runner that outputs to sys.stderr
+    test_runner = unittest.TextTestRunner()
+
+    # automatically discover all tests
+    # NOTE: only works for python 2.7 and later
+    test_suite = test_loader.discover(setup_dir+'/tests')
+
+    # run the test suite
+    test_runner.run(test_suite)
+
+try:
+    from setuptools.command.test import test
+
+    class DiscoverTest(test):
+
+        def finalize_options(self):
+            test.finalize_options(self)
+            self.test_args = []
+            self.test_suite = True
+
+        def run_tests(self):
+            discover_and_run_tests()
+
+except ImportError:
+    from distutils.core import Command
+
+    class DiscoverTest(Command):
+        user_options = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            discover_and_run_tests()
+
+import os
+
+VERSION = '0.1.0'
+setup(
+    name='pyxdump',
+    packages=setuptools.find_packages(),
+    version=VERSION,
+    description='mysql schema dump toolkit',
+    author='Allan',
+    author_email='allan_hung@trend.com.tw',
+    url='https://github.com/allanhung/pyxdump',
+    download_url='https://github.com/allanhung/pyxdump/tarball/' + VERSION,
+    keywords=['utility', 'miscellaneous', 'library'],
+    classifiers=[],
+    entry_points={                                                                                                                                                                                                                                                             
+        'console_scripts': [                                                                                                                                                                                                                                                   
+            'pyxdump = pyxdump.pyxdump:main',                                                                                                                                                                                                                          
+        ]
+    },
+    cmdclass={'test': DiscoverTest}
+)
