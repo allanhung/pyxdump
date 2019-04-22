85d4431150275f8fd665c008cea7a39c0792e06d
allan <hung.allan@gmail.com>
2019-04-22 10:18:29 +0800

    merge dump to export

--- README.md
+++ README.md
@@ -1,20 +1,34 @@
 # pyxdump
 
-MySQL schema dump tool
+MySQL export tool
 
 ## How to Build
 ```sh
-$ pip install git+https://github.com/allan-hung/pyxdump
+$ pip install -r https://raw.githubusercontent.com/allanhung/pyxdump/master/requirements.txt
+$ pip install git+https://github.com/allanhung/pyxdump
 ```
     
 ## Example
-### dump
+### export
 ```sh
-$ pyxdump dump schema --user root --password dbpass
+# export table create script and create user script
+$ pyxdump export schema --create_user
+# export table 
+$ pyxdump export table --table_list <schema1.table1,schema2.table2> --output_dir <output_dir> --remote <user@ip> --remote_cmd 'sshpass -e <cmd>'
+# export from innobackup
+$ innobackupex --no-lock --no-timestamp <backup_dir>
+# send to remote
+$ tar jcf - <backup_dir> | ssh user@ip "cat > <remotr_dir>/`date +%Y%m%d`.tar.bz2"
+# prepear to export 
+$ innobackupex --use-memory=<memory_size> --apply-log --export <backup_dir>>
 ```
 
 ### import
 ```sh
-$ pyxdump import schema --script_file /tmp/alldb.sql
-$ pyxdump import data --backupdir /dbbackup/20180604 --datadir /data/mysql
+# import schema
+$ pyxdump import schema --script_file <script_file>
+# import data from backup
+$ pyxdump import data --backupdir <backup_dir> --datadir <mysql_data_dir>
+# import data by table
+$ pyxdump import data --backupdir <backup_dir> --datadir <mysql_data_dir>  --table_list <schema1.table1,schema2.table2>
 ```
--- pyxdump/xdump_module/export.py
+++ pyxdump/xdump_module/export.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 """
-mysql export table
+mysql export
 
 Usage:
+  pyxdump export schema  [--user USER] [--password PASSWORD] [--database DATABASE] [--exclude_database EXDB] [--output_dir OUTPUTDIR] [--db_per_file] [--create_user] [--fix_row_format]
   pyxdump export table  [--user USER] [--password PASSWORD] [--table_list TBLIST] [--database DATABASE] [--exclude_database EXDB] [--output_dir OUTPUTDIR] [--remote REMOTE] [--remote_cmd REMOTECMD]
 
 Options:
@@ -13,6 +14,9 @@ Options:
   --database DATABASE       database list default export all user database (example: db1,db2)
   --exclude_database EXDB   database exclude list (example: db3,db4)
   --output_dir OUTPUTDIR    script output dir [default: /tmp]
+  --db_per_file             generate schema by database
+  --create_user             generate create user script
+  --fix_row_format          use when import 5.6 to 5.7
   --remote REMOTE           send export file to remote (example: root@10.0.0.1)
   --remote_cmd REMOTECMD    send export file to remote (example: <cmd> -i ~/mykey.pem or sshpass -e <cmd>) [default: sshpass -e <cmd>]
   -h --help                 Show this screen.
@@ -22,6 +26,47 @@ from docopt import docopt
 import subprocess
 import os
 import common
+
+def schema(args):
+    exclude_list = ['mysql' ,'information_schema', 'performance_schema', 'sys']
+    if args['--exclude_database']:
+        exclude_list.extend(args['--exclude_database'])
+    exclude_str = ''
+    if exclude_list: 
+        exclude_str = "'"+"','".join(exclude_list)+"'"
+    connect_list = []
+    if args['--user']:
+        connect_list.append('-u{0}'.format(args['--user']))
+    if args['--password']:
+        connect_list.append('-p\'{0}\''.format(args['--password']))
+    if args['--database']:
+        db_list = args['--database'].split(',')
+    else:
+        sqlscript = 'SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('+exclude_str+')'
+        db_list = (common.check_output('mysql {0} --batch --skip-column-names -e "{1}"'.format(' '.join(connect_list),sqlscript),shell=True)).strip().split('\n')
+    sqlscript = "show global variables like 'innodb_default_row_format'"
+    rlist = common.check_output('mysql {0} --batch --skip-column-names -e "{1}"'.format(' '.join(connect_list),sqlscript),shell=True).strip().split('\n')
+    if rlist[0]:
+        row_format = rlist[0].split('\t')[1]
+    else:
+        row_format = 'COMPACT'
+    if args['--db_per_file'] or len(db_list) == 1:
+        for db in db_list:
+            subprocess.call('mysqldump {0} --no-data --set-gtid-purged=OFF --force --quote-names --dump-date --opt --single-transaction --events --routines --triggers --databases {1} --result-file={2}.sql'.format(' '.join(connect_list),db,os.path.join(args['--output_dir'],db)), shell=True)
+            if args['--fix_row_format']:
+                subprocess.call('sed -i -e "s/ENGINE=InnoDB/ENGINE=InnoDB ROW_FORMAT={0}/g" {1}.sql'.format(row_format,os.path.join(args['--output_dir'],db)),shell=True)
+            print('Output File: {0}.sql'.format(os.path.join(args['--output_dir'],db)))
+            if args['--create_user']:
+                subprocess.call('pt-show-grants {0} >> {1}.sql'.format(' '.join(connect_list),os.path.join(args['--output_dir'],db)), shell=True)
+    else:
+        subprocess.call('mysqldump {0} --no-data --set-gtid-purged=OFF --force --quote-names --dump-date --opt --single-transaction --events --routines --triggers --databases {1} --result-file={2}.sql'.format(' '.join(connect_list),' '.join(db_list),os.path.join(args['--output_dir'],'alldb')), shell=True)
+        if args['--fix_row_format']:
+            subprocess.call('sed -i -e "s/ENGINE=InnoDB/ENGINE=InnoDB ROW_FORMAT={0}/g" {1}.sql'.format(row_format,os.path.join(args['--output_dir'],'alldb')),shell=True)
+        if args['--create_user']:
+            subprocess.call('pt-show-grants {0} >> {1}.sql'.format(' '.join(connect_list),os.path.join(args['--output_dir'],'alldb')), shell=True)
+        print('Output File: {0}.sql'.format(os.path.join(args['--output_dir'],'alldb')))
+
+    return None
 
 def table(args):
     exclude_list = ['mysql' ,'information_schema', 'performance_schema', 'sys']
